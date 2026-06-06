a585f50b1ab6ba53ce9b7dc58d29f9944184b7de
vodongha <vodongha@hotmail.com>
2026-06-06 18:57:37 +0700

    Add migration to re-insert Laravel skill

---
+++ Migrations/20260606115656_ReaddLaravelSkill.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vodongha.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReaddLaravelSkill : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                INSERT INTO "Skills" ("Id", "Name", "Category", "Icon", "Proficiency", "Order")
+                SELECT 4, 'Laravel', 'Backend', 'devicon-laravel-plain', 75, 4
+                WHERE NOT EXISTS (SELECT 1 FROM "Skills" WHERE "Id" = 4);
+                """);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""DELETE FROM "Skills" WHERE "Id" = 4 AND "Name" = 'Laravel';""");
+        }
+    }
+}
