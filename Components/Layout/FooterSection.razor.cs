84e123c8ba03e316b30e78282cdae64aa235e02a
vodongha <vodongha@hotmail.com>
2026-06-06 16:50:25 +0700

    Pull Name/Title/Avatar/Email/GitHub from DB settings in Hero and Footer

--- Components/Layout/FooterSection.razor.cs
+++ Components/Layout/FooterSection.razor.cs
@@ -7,13 +7,18 @@ public partial class FooterSection : ComponentBase, IDisposable
 {
     [Inject] private LanguageService Lang { get; set; } = default!;
     [Inject] private VisitorService VisitorSvc { get; set; } = default!;
+    [Inject] private SiteSettingService SettingSvc { get; set; } = default!;
 
     private int _visitorCount;
+    private Dictionary<string, string> _settings = new();
+
+    private string S(string key) => _settings.TryGetValue(key, out string? v) ? v : "";
 
     protected override async Task OnInitializedAsync()
     {
         Lang.OnChange += StateHasChanged;
         _visitorCount = await VisitorSvc.GetCountAsync();
+        _settings = await SettingSvc.GetAllAsync();
     }
 
     public void Dispose() => Lang.OnChange -= StateHasChanged;
