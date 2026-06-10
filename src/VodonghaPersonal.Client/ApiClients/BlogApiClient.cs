340f0eec18a9499cc4beba593a420400d7543ba9
vodongha <vodongha@hotmail.com>
2026-06-10 16:43:49 +0700

    refactor: extract BaseCrudApiClient to eliminate CRUD boilerplate in 5 API clients
    - Blog, Education, Experience, Project, Skill clients now extend BaseCrudApiClient<T>
    - Each subclass exposes a typed SaveAsync(T) wrapper delegating to the base

--- src/VodonghaPersonal.Client/ApiClients/BlogApiClient.cs
+++ src/VodonghaPersonal.Client/ApiClients/BlogApiClient.cs
@@ -1,25 +1,8 @@
-using System.Net.Http.Json;
 using VodonghaPersonal.Shared.Models;
 
 namespace VodonghaPersonal.Client.ApiClients;
 
-public class BlogApiClient(HttpClient http)
+public class BlogApiClient(HttpClient http) : BaseCrudApiClient<BlogPost>(http, "/api/admin/blog")
 {
-    public async Task<List<BlogPost>> GetAllAsync()
-        => await http.GetFromJsonAsync<List<BlogPost>>("/api/admin/blog") ?? [];
-
-    public async Task<BlogPost?> SaveAsync(BlogPost post)
-    {
-        HttpResponseMessage resp = post.Id == 0
-            ? await http.PostAsJsonAsync("/api/admin/blog", post)
-            : await http.PutAsJsonAsync($"/api/admin/blog/{post.Id}", post);
-        resp.EnsureSuccessStatusCode();
-        return await resp.Content.ReadFromJsonAsync<BlogPost>();
-    }
-
-    public async Task DeleteAsync(int id)
-    {
-        HttpResponseMessage resp = await http.DeleteAsync($"/api/admin/blog/{id}");
-        resp.EnsureSuccessStatusCode();
-    }
+    public Task<BlogPost?> SaveAsync(BlogPost post) => SaveAsync(post, post.Id);
 }
--- src/VodonghaPersonal.Client/ApiClients/ProjectApiClient.cs
+++ src/VodonghaPersonal.Client/ApiClients/ProjectApiClient.cs
@@ -4,29 +4,13 @@ using VodonghaPersonal.Shared.Models;
 
 namespace VodonghaPersonal.Client.ApiClients;
 
-public class ProjectApiClient(HttpClient http)
+public class ProjectApiClient(HttpClient http) : BaseCrudApiClient<Project>(http, "/api/admin/projects")
 {
-    public async Task<List<Project>> GetAllAsync()
-        => await http.GetFromJsonAsync<List<Project>>("/api/admin/projects") ?? [];
-
-    public async Task<Project?> SaveAsync(Project item)
-    {
-        HttpResponseMessage resp = item.Id == 0
-            ? await http.PostAsJsonAsync("/api/admin/projects", item)
-            : await http.PutAsJsonAsync($"/api/admin/projects/{item.Id}", item);
-        resp.EnsureSuccessStatusCode();
-        return await resp.Content.ReadFromJsonAsync<Project>();
-    }
-
-    public async Task DeleteAsync(int id)
-    {
-        HttpResponseMessage resp = await http.DeleteAsync($"/api/admin/projects/{id}");
-        resp.EnsureSuccessStatusCode();
-    }
+    public Task<Project?> SaveAsync(Project item) => SaveAsync(item, item.Id);
 
     public async Task SaveOrderAsync(List<int> ids)
     {
-        HttpResponseMessage resp = await http.PutAsJsonAsync("/api/admin/projects/order", new OrderUpdateRequest(ids));
+        HttpResponseMessage resp = await Http.PutAsJsonAsync("/api/admin/projects/order", new OrderUpdateRequest(ids));
         resp.EnsureSuccessStatusCode();
     }
 }
