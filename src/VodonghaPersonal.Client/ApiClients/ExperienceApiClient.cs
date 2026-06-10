daee5e8eb6559cf0cf30e78be6016fa9c410b51e
vodongha <vodongha@hotmail.com>
2026-06-10 16:43:49 +0700

    refactor: extract BaseCrudApiClient to eliminate CRUD boilerplate in 5 API clients
    - Blog, Education, Experience, Project, Skill clients now extend BaseCrudApiClient<T>
    - Each subclass exposes a typed SaveAsync(T) wrapper delegating to the base

--- src/VodonghaPersonal.Client/ApiClients/ExperienceApiClient.cs
+++ src/VodonghaPersonal.Client/ApiClients/ExperienceApiClient.cs
@@ -1,25 +1,8 @@
-using System.Net.Http.Json;
 using VodonghaPersonal.Shared.Models;
 
 namespace VodonghaPersonal.Client.ApiClients;
 
-public class ExperienceApiClient(HttpClient http)
+public class ExperienceApiClient(HttpClient http) : BaseCrudApiClient<Experience>(http, "/api/admin/experience")
 {
-    public async Task<List<Experience>> GetAllAsync()
-        => await http.GetFromJsonAsync<List<Experience>>("/api/admin/experience") ?? [];
-
-    public async Task<Experience?> SaveAsync(Experience item)
-    {
-        HttpResponseMessage resp = item.Id == 0
-            ? await http.PostAsJsonAsync("/api/admin/experience", item)
-            : await http.PutAsJsonAsync($"/api/admin/experience/{item.Id}", item);
-        resp.EnsureSuccessStatusCode();
-        return await resp.Content.ReadFromJsonAsync<Experience>();
-    }
-
-    public async Task DeleteAsync(int id)
-    {
-        HttpResponseMessage resp = await http.DeleteAsync($"/api/admin/experience/{id}");
-        resp.EnsureSuccessStatusCode();
-    }
+    public Task<Experience?> SaveAsync(Experience item) => SaveAsync(item, item.Id);
 }
